c3c2cfbcfe3a0e45787064e93f146784379d2fed
Dråfølin <derg@drafolin.ch>
2025-01-16 17:57:34 +0100

    cleaned up package

---
+++ src/FilamentCollapse.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Drafolin\FilamentCollapse;
+
+class FilamentCollapse {}
--- src/Testing/TestsFilamentCollapse.php
+++ src/Testing/TestsFilamentCollapse.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace VendorName\Skeleton\Testing;
+namespace Drafolin\FilamentCollapse\Testing;
 
 use Livewire\Features\SupportTesting\Testable;
 
 /**
  * @mixin Testable
  */
-class TestsSkeleton
+class TestsFilamentCollapse
 {
     //
 }
